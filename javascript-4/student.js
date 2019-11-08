22ff95e22a10b0bedf9620eef9d7c9fe15e0188e
Steven Gerard <stevengerard@Stevens-MacBook-Pro.local>
2019-11-08 16:51:33 -0700

    first commit-

--- javascript-4/student.js
+++ javascript-4/student.js
@@ -4,7 +4,7 @@
 //////////////////Step 1////////////////////
 // Create a new array called 'faveColors' and set it your three favorite colors as strings.
 
-// CODE HERE
+let faveColors = ['green', 'purple', 'black'];
 
 //////////////////Step 2////////////////////
 // Create an object called 'me' that has these keys: firstname, superHeroName, homeTown, superPowers, superPowerXP, profileImage. 
@@ -21,19 +21,39 @@
 
 // The profileImage key will return this url `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`.
 
-// CODE HERE
+let me = {
+    firstName: 'Steven',
+    superHeroName: 'Speed Demon',
+    homeTown: 'Missoula',
+    superPowers: ['speed flying', 'bitch slapping', 'crime fighter'],
+    superPowerXP: Math.floor(Math.random() * 100)
+    profileImage: 
+}
 
 //////////////////Step 3////////////////////
 // Create three variables to hold some data off your me object. The first variable should be 'regularName' that is the value of your firstName on the me object. The next variable is 'superName' which is the value of superHeroName on the me object. Last is 'homeTown' which will be the value of homeTown on the me object
 
-// CODE HERE
+let regularName = me.firstName;
+
+let superName = me.superHeroName;
+
+let homeTown = me.homeTown;
 
 //////////////////Step 4////////////////////
 //Create a function called 'setColor' that takes in one param called arr. Just to make sure that we only ever have three colors, use splice to trim the array to 3 colors. 
 
 // Next, create a for loop to loop over the array. If any of the colors you chose is 'blue', change it's value to '#4D4DFF' which is just a more appealing color of blue. Outside of the for loop but still inside of setColor, invoke the function called background which will take in three arguments. These arguments should be each item of your array. This is a function we created for you to set the background colors.
 
-// CODE HERE
+function setColor(arr){
+    arr.splice(3, 0)
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === 'blue'){
+            arr[i] = '#4D4DFF'
+        }
+    } background()
+}
+
+
 
 //////////////////Step 5////////////////////
 //Create a function called 'setPowers' that takes in arr as a parameter. Loop over the arr param and run a function called createLi(), which will take each item of the array as an agument. The createLi function is a function we created to set the data on the screen. It outside the scope of this project
